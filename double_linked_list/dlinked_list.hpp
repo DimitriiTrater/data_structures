b6cfa5f10564c5497aef0a3cf261d7590da9e442
DimitriiTrater <dima1603040@gmail.com>
2023-05-10 15:20:14 +0200

    add node in dll

--- double_linked_list/dlinked_list.hpp
+++ double_linked_list/dlinked_list.hpp
@@ -2,10 +2,26 @@
 #define DLINKED_LIST_HPP
 
 
+#include <type_traits>
+
+
+template<class T>
 class DLinkedList
 {
 
 
+struct Node
+{
+    int data;
+    Node* next;
+    Node* prev;
+    Node(T data=T(), Node* next=nullptr, Node* prev=nullptr) : data(data), next(next), prev(prev) {}
+
+};
+
+public:
+    DLinkedList();
+    ~DLinkedList();
 };
 
 #endif // DLINKED_LIST_HPP
