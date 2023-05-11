b954499fa9a5c818ed9148c3bfd23b50b41cadd4
DimitriiTrater <dima1603040@gmail.com>
2023-05-11 15:28:37 +0200

    add ctor in string

--- string_ds/string_ds.hpp
+++ string_ds/string_ds.hpp
@@ -1,13 +1,24 @@
 #ifndef STRING_DS_HPP
 #define STRING_DS_HPP
 
+#include <cstring>
 class string
 {
 
-char *str;
+char* _str;
+int _len;
+
 
 public:
-    string();
+    string(): _str(nullptr) {}
+
+    string(const char* str) : _str(new char[strlen(str)+1]), _len(strlen(str))
+    {
+        for (int i = 0; i < _len; i++)
+            _str[i] = str[i];
+        _str[_len] = '\0';
+    }
+
     ~string();
 };
 
