1490c08fcb21cfc7b2bc9de5d3259668dd65b826
DimitriiTrater <dima1603040@gmail.com>
2023-05-22 15:25:24 +0200

    add push pop

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -1,14 +1,70 @@
 
-
+#include <exception>
+#include <stdexcept>
+template<class T>
 class Stack
 {
     struct Node
     {
-        int value;
+        T value;
         Node* next;
+        Node(T value=T(), Node* next=nullptr) : value(value), next(next)
+        {}
     };
 
+    const unsigned int STACK_CAPACITY = 10000;
+
+    unsigned int _size;
+    unsigned int _capacity;
+    Node* top;
 public:
     Stack();
     ~Stack();
+
+    void push(T value);
+    void pop();
+    T peek();
+
+
+    unsigned int size() const {return _size;}
+    unsigned int capacity() const {return _capacity;}
 };
+
+
+template<class T>
+Stack<T>::Stack() : top(), _size(0), _capacity(STACK_CAPACITY)
+{}
+
+template<class T>
+Stack<T>::~Stack()
+{}
+
+
+template<class T>
+void Stack<T>::push(T value)
+{
+    _size++;
+    if (!top)
+    {
+        Node* temp = new Node(value, top);
+        top = temp;
+        return;
+    }
+    top = new Node(value);
+
+}
+
+template<class T>
+void Stack<T>::pop()
+{
+    if (!top) throw std::out_of_range("Error: Out of range");
+
+    Node* temp = top;
+    top = top->next;
+    delete temp;
+    _size--;
+}
+
+template<class T>
+T Stack<T>::peek()
+{}
